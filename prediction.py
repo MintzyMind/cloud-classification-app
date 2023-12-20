7e997fe2672ae11bb77e18e9b50371a7bdea1e43
MintzyMind <140625046+MintzyMind@users.noreply.github.com>
2023-12-21 01:32:32 +0700

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -9,7 +9,6 @@ def pred_class(model: torch.nn.Module,
                class_names: List[str],
                image_size: Tuple[int, int] = (224, 224),
                ):
-    
     # 2. Open image
     img = image
 
@@ -21,26 +20,28 @@ def pred_class(model: torch.nn.Module,
                     std=[0.229, 0.224, 0.225]),
     ])
 
-    # 4. Make sure the model is on the target device
+    ### Predict on image ###
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # 4. Make sure the model is on the target device
     model.to(device)
 
-    # 5. Turn on model evaluation mode and inference mode
+    # 5. Turn on model evaluation mode and use torch.no_grad() for inference
     model.eval()
-    with torch.inference_mode():
-        # 6. Transform and add an extra dimension to image (model requires samples in [batch_size, color_channels, height, width])
+    with torch.no_grad():
+        # 6. Transform and add an extra dimension to the image
         transformed_image = image_transform(img).unsqueeze(dim=0).float()
 
         # 7. Make a prediction on the image with an extra dimension and send it to the target device
         target_image_pred = model(transformed_image.to(device))
 
-        # 8. Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
-        target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
+    # 8. Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
+    target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
 
-        # 9. Convert prediction probabilities -> prediction labels
-        target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1).item()
+    # 9. Convert prediction probabilities -> prediction labels
+    target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
 
-        classname = class_names[target_image_pred_label]
-        prob = target_image_pred_probs[0, target_image_pred_label].item()
+    classname = class_names[target_image_pred_label]
+    prob = target_image_pred_probs.cpu().numpy()
 
     return classname, prob
