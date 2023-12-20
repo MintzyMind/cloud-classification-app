f0e4f93aa1bab3c0eb471031e2fce19dc6f3ce33
MintzyMind <140625046+MintzyMind@users.noreply.github.com>
2023-12-21 01:41:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import torch
 from PIL import Image
 from prediction import pred_class
 import numpy as np
+from torchvision.models import mobilenet_v3_large
+
 
 # Set title
 st.title('Cloud Classification')
@@ -28,7 +30,7 @@ if uploaded_image is not None:
     image = Image.open(uploaded_image).convert('RGB')
     st.image(image, caption='Uploaded Image', use_column_width=True)
 
-    class_name = ['Altocumulus', 'Altostratus', 'Cirrocumulus', 'Cirrostratus', 'Cirrus', 'Contrails', 'Cumulonimbus', 'Cumulus', 'Nimbostratus', 'Stratocumulus', 'Stratus']
+    class_names = ['Altocumulus', 'Altostratus', 'Cirrocumulus', 'Cirrostratus', 'Cirrus', 'Contrails', 'Cumulonimbus', 'Cumulus', 'Nimbostratus', 'Stratocumulus', 'Stratus']
 
     if st.button('Prediction'):
 
@@ -36,15 +38,15 @@ if uploaded_image is not None:
             st.error("Model not loaded successfully. Please check the model file.")
         else:
             # Prediction class
-            probli = pred_class(model, image, class_name)
+            probli = pred_class(model, image, class_names)
 
             st.write("## Prediction Result")
             # Get the index of the maximum value in probli[0]
             max_index = np.argmax(probli[0])
 
             # Iterate over the class_name and probli lists
-            for i in range(len(class_name)):
+            for i in range(len(class_names)):
                 # Set the color to blue if it's the maximum value, otherwise use the default color
                 color = "blue" if i == max_index else "black"
                 st.write(
-                    f"## <span style='color:{color}'>{class_name[i]} : {probli[0][i] * 100:.2f}%</span>", unsafe_allow_html=True)
+                    f"## <span style='color:{color}'>{class_names[i]} : {probli[0][i] * 100:.2f}%</span>", unsafe_allow_html=True)
